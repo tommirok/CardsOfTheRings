d0b2620a6b9921eaa7cc6912bfb07b2b2cf1f899
Tommi Rokolampi <tommi.rokolampi@ssh.com>
2023-11-29 22:51:37 +0200

    header and api functions

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+const BaseUrl: string = 'https://ringsdb.com/api/public';
+export const imageBaseUrl: string = 'https://ringsdb.com';
+
+import { Card, Deck } from '../types';
+
+const getDeckById = async (deckId: number): Promise<Deck> =>
+  await fetch(`${BaseUrl}/decklist/${deckId}`).then((res) => res.json());
+
+export const getCards = async (deckId: number): Promise<Card[]> => {
+  const deck = await getDeckById(deckId);
+
+  const cardsCodes = Object.keys(deck.heroes);
+  const cards = cardsCodes.map(
+    async (code) =>
+      await fetch(`${BaseUrl}/card/${code}`).then((res) => res.json())
+  );
+  const cardsArr: Card[] = await Promise.all(cards);
+  console.log(cardsArr, 'cards');
+
+  return cardsArr;
+};
---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  title: string;
+};
+const Header = (props: Props) => {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  );
+};
+
+export default Header;
